ec65f5128c846818db2b037f2d265e3b81484451
zkyy484 <dzakymusyaddad@gmail.com>
2024-10-24 22:13:15 +0700

    uts done

---
+++ android/app/src/main/kotlin/com/example/projek_uts/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.projek_uts
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:projek_uts/screens/halaman_daftar.dart';
+import 'package:projek_uts/screens/halaman_masuk.dart';
+import 'package:projek_uts/screens/halaman_profil.dart';
+import 'package:projek_uts/screens/item.dart';
+import 'package:projek_uts/screens/welcome.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Moch. Dzaky Musyaddad',
+      home: Welcome(),
+      debugShowCheckedModeBanner: false,
+       
+      routes: {
+        '/daftar' : (context) => DaftarPage(),
+        '/masuk' : (context) => MasukPage(atribut: Atribut(name: 'Moch. Dzaky Musyaddad', email: ''),),
+        '/profil' : (context) {
+          Atribut atribut = Atribut(name: 'Moch. Dzaky Musyaddad', email: 'dzakymusyaddad@gmail.com');
+          return ProfilPage(atribut: atribut);
+        },
+
+        },
+    );
+  }
+}
+
